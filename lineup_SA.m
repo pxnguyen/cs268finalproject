7af1137c0a7de96784cedef4729d076cdc78378d
Yangsilu <yangsilulouis@gmail.com>
2015-12-08 12:50:39 -0800

    Add position Check; kill global usage

--- lineup_SA.m
+++ lineup_SA.m
@@ -3,9 +3,10 @@ function res=lineup_SA(info, history, salary, avail, opts)
 % strategy: get the big 3 players, share the average
     res = cell(8, 1);
     names = info.names(avail);
-    positions = info.positions(avail);
-    playerAvailable = avail(avail);
-
+    
+    pkg.positions = info.positions(avail);
+    %a=unique(pkg.positions,'stable')
+    %b=cellfun(@(x) sum(ismember(pkg.positions,x)),a,'un',0)
     fp_projection = history.fantasypoint;
     fp_projection(isnan(fp_projection)) = 0;
     total = sum(fp_projection, 2);
@@ -13,66 +14,90 @@ function res=lineup_SA(info, history, salary, avail, opts)
     fp_projection = total ./ (playCount +eps);
     pfp = fp_projection(avail);
     % fp_projection = fp_projection(:,end); % average projection
-    global salaryA
-    salaryA = salary(avail);
-    global setCost
-    setCost = opts.salarycap;
-    values = (pfp * 1000) ./ (salaryA+eps);
-    pfp_avg = mean(pfp, 2); % average projection
     
-    tmp_pfp = sort(pfp_avg);
-    global maxFP
-    maxFP = sum(tmp_pfp(end-7:end));
+    pkg.salaryA = salary(avail);
+    pkg.setCost = opts.salarycap;
+    values = (pfp * 1000) ./ (pkg.salaryA+eps);
+    pkg.pfp_avg = mean(pfp, 2); % average projection
     
-    global availN
-    availN = length(avail);
+    tmp_pfp = sort(pkg.pfp_avg);
+    
+    pkg.maxFP = sum(tmp_pfp(end-7:end));
+    
+    pkg.availN = length(avail);
 
-    global coolRate
-    coolRate = 0.95;
+    pkg.coolRate = 0.95;
     
-    initial_indices = 1:1:8
-    xRes = sA(initial_indices, maxFP, 1, 0,  pfp_avg)
+    initial_indices = 1:1:8;
+    xRes = sA(initial_indices, pkg.maxFP, 1, 0, pkg);
     res = names(xRes);
 end
 
-function [xNew] = sA(xOld, Tmax, epsDeltaT, Tmin,  pfp_avg)
+function [xNew] = sA(xOld, Tmax, epsDeltaT, Tmin,  pkg)
     T = Tmax;
     step = 0;
     delta = 10;
     counter = 0;
-    while (delta > epsDeltaT && T > Tmin && counter<100)
+    while (delta > epsDeltaT && T > Tmin && counter<10)
         counter = counter + 1
-        energyVal(xOld, pfp_avg)
-        xNew = acceptN(xOld, T, 10, pfp_avg);
-        global coolRate;
-        T = T * coolRate;
+        energyVal(xOld, pkg)
+        xNew = acceptN(xOld, T, 10, pkg);
+        T = T * pkg.coolRate;
         step = step + 1;
         
-        delta = abs(energyVal(xOld, pfp_avg)-energyVal(xNew, pfp_avg));
+        delta = abs(energyVal(xOld, pkg)-energyVal(xNew, pkg));
         xOld = xNew;
         
     end
 end
 
-function [xNew] = flip(xOld, pfp_avg)
+function [xNew] = flip(xOld, pkg)
    isOK = false;
    while(~isOK)
-      xNew = flipOne(xOld, pfp_avg);
+      xNew = flipOne(xOld, pkg);
       
-    global salaryA
-      totalCost = sum(salaryA(xNew));
-      
-    global setCost
-      isOK = totalCost < setCost;
+        totalCost = sum(pkg.salaryA(xNew));
+        costOK = totalCost < pkg.setCost;
+%         costOK
+
+        tmpPos = pkg.positions(xNew);
+        pgCnt = 0;
+        sgCnt = 0;
+        pfCnt = 0;
+        sfCnt = 0;
+        cCnt = 0;
+        for i=1:length(tmpPos)
+            
+            switch tmpPos{i}
+                case 'PG'
+                    pgCnt = pgCnt+1;
+                case 'SG'
+                    sgCnt = sgCnt+1;
+                case 'PF'
+                    pfCnt = pfCnt+1;
+                case 'SF'
+                    sfCnt = sfCnt+1;
+                otherwise
+                    cCnt = cCnt+1;
+            end
+        end
+        gCnt = pgCnt+sgCnt;
+        fCnt = pfCnt+sfCnt;
+
+        posOK = (pgCnt>=1 && sgCnt>=1 && pfCnt>=1 && sfCnt>=1 && cCnt>=1 && gCnt>=3 && fCnt>=3);
+%         posOK
+        
+        %fprintf('------\n');
+        isOK = posOK && costOK;
+        
    end
 end
 
 
-function [xNew] = flipOne(xOld, pfp_avg)
-   global availN
-   steps = ceil( (availN-8) * rand(1));
+function [xNew] = flipOne(xOld, pkg)
+   steps = ceil( (pkg.availN-8) * rand(1));
    candInd = ceil(8*rand(1));
-   for i=1:length(pfp_avg)
+   for i=1:length(pkg.pfp_avg)
       if (ismember(i, xOld)==0) 
           steps = steps-1;
       end
@@ -83,10 +108,10 @@ function [xNew] = flipOne(xOld, pfp_avg)
    end
    xNew = xOld;
 end
-function [x] = acceptN(xOld, T, innerIter, pfp_avg)
+function [x] = acceptN(xOld, T, innerIter, pkg)
     for k = 1:innerIter
-        xNew = flip(xOld, pfp_avg);
-        deltaE = energyVal(xOld, pfp_avg) - energyVal(xNew, pfp_avg);
+        xNew = flip(xOld, pkg);
+        deltaE = energyVal(xOld, pkg) - energyVal(xNew, pkg);
         if (deltaE > 0)
             xOld = xNew;
         else
@@ -98,14 +123,14 @@ function [x] = acceptN(xOld, T, innerIter, pfp_avg)
     x = xOld;
 end
 
-function [v] = energyVal(x, pfp_avg)
+function [v] = energyVal(x, pkg)
 %UNTITLED Summary of this function goes here
     %coloring;
     v = 0;
     for i = 1:length(x)
-        v = v + pfp_avg(x(i));
+        v = v + pkg.pfp_avg(x(i));
     end
-    global maxFP
-    v = maxFP - v;
+    v = pkg.maxFP - v;
 end
+
 
